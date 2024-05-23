20134b3dcfb27c0b2e3b7c15d13e098ddf100769
Rasha-Darawsheh <darawsheh.rasha@outlook.com>
2024-05-23 11:53:46 +0300

    Add files via upload
    This code is my first trial at C#!

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Bank_of_Questions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Questions Q1 = new Questions();
+        Q1.SetQuestionsText("what is 5*5");
+
+        Q1.AddOptions("25");
+        Q1.AddOptions("20");
+        Q1.AddOptions("26");
+        Q1.AddOptions("35");
+        Q1.AddOptions("37");
+
+        Q1.SetCorrectAnswer("25");
+
+        Q1.PrintQuestionsText();
+        Console.WriteLine($"options: \n");
+
+        int count = 1;
+        foreach (string item in Q1.Options)
+        {
+            Console.WriteLine(count++ + " " + item);
+        }
+
+        Console.WriteLine("Please enter the correct answer  ");
+
+        string answerString = Console.ReadLine();
+        if (Q1.CorrectAnswer == answerString)
+        {
+            Console.WriteLine("CorrectAnswer");
+        }
+        else
+        {
+            for (int i = 0; i < Q1.Options.Count; i++)
+            {
+                if (int.TryParse(answerString, out int answerInt))
+                {
+                    int indexOfUserAnswer = (answerInt - 1);
+
+                    if (indexOfUserAnswer == i)
+                    {
+                        if (Q1.Options[i] == Q1.CorrectAnswer)
+                        {
+                            Console.WriteLine("Correct Answer");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+}   
---
+++ Questions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_of_Questions
+{
+    public class Questions
+    {
+        public string QuestionText { get; set; }
+        public List<string> Options { get; set; } = new List<string>();
+        public string CorrectAnswer { get; set; }
+
+        public void SetQuestionsText(string value)
+        {
+            QuestionText = value;
+        }
+        public void PrintQuestionsText()
+        {
+            Console.WriteLine($"Question: {QuestionText}");
+        }
+        public void AddOptions(string options)
+        {
+            Options.Add(options);
+        }
+        public void SetCorrectAnswer(string answer)
+        {
+            foreach (string item in Options)
+            {
+                if (item == answer)
+                {
+                    CorrectAnswer = answer;
+                    break;
+                }
+            }
+
+
+        }
+        public void PrintOptions()
+        {
+            int count = 0;
+            foreach (string item in Options)
+            {
+                Console.WriteLine(count++ + " " + item);
+            }
+
+
+        }
+
+
+
+
+    }
+
+}
